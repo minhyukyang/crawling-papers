e65a624899803364472aeaf96a4515b36e0fa562
minhyuk.yang <minhyuk.yang@gmail.com>
2021-01-15 17:12:20 +0900

    Modified file

--- src/dbpia.R
+++ src/dbpia.R
@@ -1,8 +1,10 @@
-# - ref. : https://github.com/chanhee-kang/DBpia_crawler/blob/master/dbpp.py
+# - ref1 : https://github.com/chanhee-kang/DBpia_crawler/blob/master/dbpp.py
+# - ref2 : https://wikidocs.net/67127
 # # java -jar selenium-server-standalone-3.141.59.jar -port 4445
 
 library(RSelenium)
 library(rvest)
+library(dplyr)
 
 searchQ <- ""
 startYear <- 2010
@@ -62,22 +64,24 @@ elem_period_click_button$clickElement()
 # '더보기' 확장 -> 마지막 페이지 까지
 more_count <- 0
 
-while(TRUE){
-  Sys.sleep(1) # time.sleep(4) 
-  tryCatch({
-    click_more <- remDr$findElement(using='xpath', value='//*[@id="contents"]/div[2]/div[3]/div[3]/div[3]/div/a')
+tryCatch({
+  while (TRUE) {
+    Sys.sleep(1) # time.sleep(4)
+    
+    click_more <- remDr$findElement(using = 'xpath', value = '//*[@id="contents"]/div[2]/div[3]/div[3]/div[3]/div/a')
     remDr$mouseMoveToLocation(webElement = click_more)
     remDr$click(buttonId = 'LEFT')
     # click_more$clickElement() # driver.find_element_by_xpath(xpath).click()
-  }, error = function(e){
-    # print(e)
-    print("done.")
-    break
-  })
-  more_count <- more_count + 1
-  # sprintf(" + page [%d] ", more_count)  
-  print(more_count)  
-}
+    
+    more_count <- more_count + 1
+    # sprintf(" + page [%d] ", more_count)
+    print(more_count)
+  }
+}, error = function(e) {
+  # print(e)
+  print("done.")
+  break
+})
 
 # `$clickElement()`로 동작하지 않을 때
 # remDr$mouseMoveToLocation(webElement = elem_period_click_button)
@@ -89,15 +93,77 @@ while(TRUE){
 items_source <- remDr$getPageSource()[[1]]
 items_source_html <- read_html(items_source)
 #dev_search_list > li:nth-child(487)
+#dev_search_list > li:nth-child(481)
 
-list_title <- items_source_html %>% html_nodes("#dev_search_list > li") %>% html_nodes(".titWrap") %>% html_text()
-list_author <- items_source_html %>% html_nodes("#dev_search_list > li") %>% html_nodes(".author") %>% html_text()
-list_publisher <- items_source_html %>% html_nodes("#dev_search_list > li") %>% html_nodes(".publisher") %>% html_text()
-list_journal <- items_source_html %>% html_nodes("#dev_search_list > li") %>% html_nodes(".journal") %>% html_text()
-list_volume <- items_source_html %>% html_nodes("#dev_search_list > li") %>% html_nodes(".volume") %>% html_text()
-list_date <- items_source_html %>% html_nodes("#dev_search_list > li") %>% html_nodes(".date") %>% html_text()
-# list_abstract 
+items <- items_source_html %>% html_nodes("#dev_search_list > li")
 
+# 논문제목, 저자, 퍼블리셔, 저널명,볼륨,날짜,초록
+items_cnt <-  length(items)
+results <- tibble()
+
+print("start parsing")
+
+for (i in 1:items_cnt){
+  
+  
+  if (i %% 10 == 0){
+    sprintf(" parsing.. [%d/%d]", i, items_cnt)
+  }
+  
+  tryCatch({
+    title <- items_source_html %>% html_nodes("#dev_search_list > li") %>% html_nodes(".titWrap") %>% html_nodes("h5 > a") %>% html_text() %>% .[i]
+  }, error = function(e){
+    title <- ''
+  })
+  
+  tryCatch({
+    author <- items_source_html %>% html_nodes("#dev_search_list > li") %>% html_nodes(".author") %>% html_text() %>% .[i]
+  }, error = function(e){
+    author <- ''
+  })
+  
+  tryCatch({
+    publisher <- items_source_html %>% html_nodes("#dev_search_list > li") %>% html_nodes(".publisher") %>% html_text() %>% .[i]
+  }, error = function(e){
+    publisher <- ''
+  })
+  
+  tryCatch({
+    journal <- items_source_html %>% html_nodes("#dev_search_list > li") %>% html_nodes(".journal") %>% html_text() %>% .[i]
+  }, error = function(e){
+    journal <- ''
+  })
+  
+  tryCatch({
+    volume <- items_source_html %>% html_nodes("#dev_search_list > li") %>% html_nodes(".volume") %>% html_text() %>% .[i]
+  }, error = function(e){
+    volume <- ''
+  })
+  
+  tryCatch({
+    date <- items_source_html %>% html_nodes("#dev_search_list > li") %>% html_nodes(".date") %>% html_text() %>% .[i]
+  }, error = function(e){
+    date <- ''
+  })
+  
+  # 논문 다운로드 구현
+  # item 별로 url를 추출하여 '다운로드' 버튼 클릭
+
+  tmp <- tibble(
+    "title" = as.character(title),
+    "author" = as.character(author),
+    "publisher" = as.character(publisher),
+    "journal" = as.character(journal),
+    "volume" = as.character(volume),
+    "date" = as.character(date)
+  )
+  
+  results <- bind_rows(results, tmp)
+  
+  print(results[i,])
+}
+
+print(results)
 
 # 잔여 작업 -------------------------------------------------------------------
 
