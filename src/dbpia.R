564feda0721b4b3407cd0ff0f6134f5e904aebe2
minhyukyang <minhyuk.yang@gmail.com>
2021-01-20 17:21:21 +0900

    Modified file

--- src/dbpia.R
+++ src/dbpia.R
@@ -1,23 +1,50 @@
+## RSelenium 사전 설치
+# 1. 사전 환경 세팅 -------------------------------------------------------------
+#
+# 1) chromedriver 설치
+# - 링크 : https://sites.google.com/a/chromium.org/chromedriver/
+# * 현재 설치된 chrome 버전에 맞는 드라이버 다운로드
+#
+# 2) selenium 설치
+# - 프로그램을 이용해 자동화된 웹 테스트를 수행할 수 있도록 해주는 프레임 워크 (https://namu.wiki/w/Selenium)
+# - 다운로드 링크 : https://www.seleniumhq.org/download/
+# * Selenium Server (Grid) > stable version 받기
+#
+# 3) 설치
+# 1), 2)를 c:\rselenium 폴더에 넣기
+# chrome drive는 동일 폴더에 압출 해제 하기
+#
+# 4) Selenium 서버 띄우기 
+# 명령 프롬프트 실행 (단축키 : Ctrl + R > "cmd")
+# 설치 폴더로 이동 : "cd c:\rselenium"
+# 명령어 입력 : "java -jar selenium-server-standalone-3.141.59.jar -port 4445"
+# -> "Selenium Server is up and running on port 4445" 이란 로그가 나타나면 성공
+
+# DBPia Cralwer 참고
 # - ref1 : https://github.com/chanhee-kang/DBpia_crawler/blob/master/dbpp.py
 # - ref2 : https://wikidocs.net/67127
 # # java -jar selenium-server-standalone-3.141.59.jar -port 4445
 
+
+# 0. 환경세팅 -----------------------------------------------------------------
+
 library(RSelenium)
 library(rvest)
 library(dplyr)
+library(stringr)
+library(tidyverse)
+library(tictoc)
 
-searchQ <- ""
-startYear <- 2010
-endYear <- 2021
-  
 print("start crawling..")
+
+### chrome driver 실행
 
 remDr <- remoteDriver(
   remoteServerAddr="localhost",
   port=4445L,
   browserName="chrome")
 
-remDr$open()
+remDr$open() # chrome 창 생성
 
 # 기본 설정값
 dbpia_url <- "https://www.dbpia.co.kr/"
@@ -40,7 +67,7 @@ elem_keyword_id$sendKeysToElement(list(keyword)) # elem_login.send_keys('ID')
 elem_search_button <- remDr$findElement(using='xpath', value='//*[@id="header"]/div[5]/div[6]/div[2]/div[1]/a')
 elem_search_button$clickElement() # driver.find_element_by_xpath(xpath).click() 
 
-# 기간 지정 및 체크박스 설정
+# 기간 지정 
 elem_start_year <- remDr$findElement(using='xpath', value='//*[@id="dev_sartYY"]')
 elem_start_year$clickElement()
 elem_start_year$clearElement()
@@ -54,20 +81,23 @@ elem_end_year$sendKeysToElement(list(end_year))
 elem_period_click_button <- remDr$findElement(using='xpath', value='//*[@id="sidebar"]/form/div[3]/div/div[1]/ul/li[4]/p/button')
 elem_period_click_button$clickElement()
 
-# '자료유형' 체크박스 선택
+# '자료유형' 체크박스 선택 // 미구현
 # - 학술대회자료, 학술저널 등
 # elem_pub_check_1 <- remDr$findElement(using='xpath', value='//*[@id="pub_check_sort3_0"]')
 # elem_pub_check_1$clickElement() # driver.find_element_by_xpath(xpath).click()
 # elem_pub_check_2 <- remDr$findElement(using='xpath', value='//*[@id="dev_plctType"]/li[1]/span/label')
 # elem_pub_check_2$clickElement() # driver.find_element_by_xpath(xpath).click()
- 
+
 # '더보기' 확장 -> 마지막 페이지 까지
 more_count <- 0
 
-tryCatch({
-  while (TRUE) {
-    Sys.sleep(1) # time.sleep(4)
-    
+# error 처리
+# - https://stackoverflow.com/questions/53497343/r-cannot-break-the-while-loop-with-use-of-trycatch
+
+while (TRUE) {
+  Sys.sleep(1) # time.sleep(4)
+  
+  res <- try({
     click_more <- remDr$findElement(using = 'xpath', value = '//*[@id="contents"]/div[2]/div[3]/div[3]/div[3]/div/a')
     remDr$mouseMoveToLocation(webElement = click_more)
     remDr$click(buttonId = 'LEFT')
@@ -75,13 +105,14 @@ tryCatch({
     
     more_count <- more_count + 1
     # sprintf(" + page [%d] ", more_count)
-    print(more_count)
+  }, silent = TRUE)
+  if (inherits(res, "try-error")){
+    message("done")
+    break
+  } else {
+    message(more_count)
   }
-}, error = function(e) {
-  # print(e)
-  print("done.")
-  break
-})
+}
 
 # `$clickElement()`로 동작하지 않을 때
 # remDr$mouseMoveToLocation(webElement = elem_period_click_button)
@@ -97,77 +128,123 @@ items_source_html <- read_html(items_source)
 
 items <- items_source_html %>% html_nodes("#dev_search_list > li")
 
-# 논문제목, 저자, 퍼블리셔, 저널명,볼륨,날짜,초록
+# 논문제목, 저자, 퍼블리셔, 저널명, 볼륨, 날짜, 초록, 다운로드ID(논문)
 items_cnt <-  length(items)
 results <- tibble()
 
 print("start parsing")
 
 for (i in 1:items_cnt){
-  
+  # for (i in 1:5){
   
   if (i %% 10 == 0){
     sprintf(" parsing.. [%d/%d]", i, items_cnt)
   }
   
   tryCatch({
-    title <- items_source_html %>% html_nodes("#dev_search_list > li") %>% html_nodes(".titWrap") %>% html_nodes("h5 > a") %>% html_text() %>% .[i]
+    title <- items_source_html %>% html_nodes("#dev_search_list > li") %>% .[i] %>% html_nodes(".titWrap") %>% html_nodes("h5 > a") %>% html_text()
+    title <- ifelse(length(title) == 0, '', title)
   }, error = function(e){
     title <- ''
   })
   
   tryCatch({
-    author <- items_source_html %>% html_nodes("#dev_search_list > li") %>% html_nodes(".author") %>% html_text() %>% .[i]
+    author <- items_source_html %>% html_nodes("#dev_search_list > li") %>% .[i] %>% html_nodes(".author") %>% html_text()
+    author <- ifelse(length(author) == 0, '', author)
   }, error = function(e){
     author <- ''
   })
   
   tryCatch({
-    publisher <- items_source_html %>% html_nodes("#dev_search_list > li") %>% html_nodes(".publisher") %>% html_text() %>% .[i]
+    publisher <- items_source_html %>% html_nodes("#dev_search_list > li") %>% .[i] %>% html_nodes(".publisher") %>% html_text() 
+    publisher <- ifelse(length(publisher) == 0, '', publisher)
   }, error = function(e){
     publisher <- ''
   })
   
   tryCatch({
-    journal <- items_source_html %>% html_nodes("#dev_search_list > li") %>% html_nodes(".journal") %>% html_text() %>% .[i]
+    journal <- items_source_html %>% html_nodes("#dev_search_list > li") %>% .[i] %>% html_nodes(".journal") %>% html_text() 
+    journal <- ifelse(length(journal) == 0, '', journal)
   }, error = function(e){
     journal <- ''
   })
   
   tryCatch({
-    volume <- items_source_html %>% html_nodes("#dev_search_list > li") %>% html_nodes(".volume") %>% html_text() %>% .[i]
+    volume <- items_source_html %>% html_nodes("#dev_search_list > li") %>% .[i] %>% html_nodes(".volume") %>% html_text() 
+    volume <- ifelse(length(volume) == 0, '', volume)
   }, error = function(e){
     volume <- ''
   })
   
   tryCatch({
-    date <- items_source_html %>% html_nodes("#dev_search_list > li") %>% html_nodes(".date") %>% html_text() %>% .[i]
+    date <- items_source_html %>% html_nodes("#dev_search_list > li") %>% .[i] %>% html_nodes(".date") %>% html_text()
+    date <- ifelse(length(date) == 0, '', date)
   }, error = function(e){
     date <- ''
   })
   
-  # 논문 다운로드 구현
-  # item 별로 url를 추출하여 '다운로드' 버튼 클릭
-
+  tryCatch({
+    download_id <- items_source_html %>% html_nodes("#dev_search_list > li") %>% .[i] %>% html_nodes("div > div > p > button") %>% html_attr("id") %>% tibble() %>% drop_na() %>% pull()
+    download_id <- ifelse(length(download_id) == 0, '', download_id)
+  }, error = function(e){
+    download_id <- ''
+  })
+  
   tmp <- tibble(
-    "title" = as.character(title),
-    "author" = as.character(author),
-    "publisher" = as.character(publisher),
-    "journal" = as.character(journal),
-    "volume" = as.character(volume),
-    "date" = as.character(date)
+    id = as.character(i),
+    title = as.character(title),
+    author = as.character(author),
+    publisher = as.character(publisher),
+    journal = as.character(journal),
+    volume = as.character(volume),
+    date = as.character(date),
+    download_id = as.character(download_id)
   )
+  print(tmp)
   
+  # results <- rbind(results, tmp)
   results <- bind_rows(results, tmp)
-  
-  print(results[i,])
+  # print(results[i,])
 }
 
 print(results)
 
-# 잔여 작업 -------------------------------------------------------------------
+#download_NODE10507560
+#download_NODE10496242
 
-# 초록 추출
-# 논문 다운로드 구현
-# 논문 다운로드 후 PDF 추출출
+# 3. 논문 다운로드 --------------------------------------------------------------
 
+# extract download list
+# items_source_html %>% html_nodes("#dev_search_list > li") %>% .[i] %>% html_nodes("div > div > p > button") %>% html_attr("id") %>% tibble() %>% drop_na() %>% pull()
+
+# download_list <- items_source_html %>% html_nodes("#dev_search_list > li > div > div > p > button") %>% html_attr("id") %>% tibble() %>% drop_na() %>% pull()
+# length(download_list)
+
+# 다운로드 버튼이 없는 자료 제외
+download_list <- results %>% filter(download_id != '')
+nrow(download_list)
+
+print("Download papers...")
+
+tic()
+for(i in 1:nrow(download_list)){
+  
+  download_id <- capture.output(cat(paste0("//*[@id='",download_list$download_id[i],"']")))
+  download_btn <- remDr$findElement(using="xpath", value=download_id)
+  # download_btn <- remDr$findElement(using='xpath', value='//*[@id="download_NODE10507560"]')
+  download_btn$clickElement()
+  
+  cat("\n[",i,"/",nrow(download_list),"] ", download_list$title[i], sep="")
+  Sys.sleep(3) # time.sleep(4)
+  
+  tryCatch({
+    recommend_btn <-  remDr$findElement(using="xpath", value="//*[@id='pub_modalRecommendThesis']/div/div[1]/button")
+    recommend_btn$clickElement()  
+  }, error = function(e){
+    next
+  })
+  Sys.sleep(2) # time.sleep(4)  
+}
+toc()
+
+print("Complete...")
